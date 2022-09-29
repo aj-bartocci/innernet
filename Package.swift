54f8ff93fc1131fb60ba39d932cd2d2e2ffd0ebc
AJ Bartocci <ajbartocci@yahoo.com>
2022-09-29 10:41:01 -0400

    pin InnernetBase to exact version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,11 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "InnernetBase", url: "https://github.com/aj-bartocci/innernet-base", from: "0.0.1")
+        .package(
+            name: "InnernetBase",
+            url: "https://github.com/aj-bartocci/innernet-base",
+            .exact("0.0.2")
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
